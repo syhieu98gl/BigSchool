addc8c028975cad0c30fd12119be7a8d9d5721bc
syhieu98gl <syhieu1998gl@gmail.com>
2019-03-28 11:13:46 +0700

    18

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using BigSchool.Models;
+using BigSchool.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
--- BigSchool/Models/Course.cs
+++ BigSchool/Models/Course.cs
@@ -12,6 +12,13 @@ namespace BigSchool.Models
 
         public ApplicationUser Lecturer { get; set; }
         [Required]
-        public 
+        public string Lecturerid { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
     }
 }

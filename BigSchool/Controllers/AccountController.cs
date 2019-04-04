356bccfe3c287354f8f4766debcfad21ead33cbb
syhieu98 <syhieu1998gl@gmail.com>
2019-04-04 11:19:41 +0700

    a

--- BigSchool/Controllers/AccountController.cs
+++ BigSchool/Controllers/AccountController.cs
@@ -208,7 +208,7 @@ namespace BigSchool.Controllers
                     // Don't reveal that the user does not exist or is not confirmed
                     return View("ForgotPasswordConfirmation");
                 }
-                //
+                ////
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 // Send an email with this link
                 // string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
